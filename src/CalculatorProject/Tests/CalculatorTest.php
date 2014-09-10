6170e10ad08447c1dd1e79f88ed63b78b8841598
jeslopcru <jeslopcru@gmail.com>
2014-09-10 23:52:37 +0200

    refactor tests

--- src/CalculatorProject/Tests/CalculatorTest.php
+++ src/CalculatorProject/Tests/CalculatorTest.php
@@ -6,17 +6,22 @@ use CalculatorProject\Calculator;
 
 class CalculatorTest extends \PHPUnit_Framework_TestCase
 {
+    /** @var  Calculator */
+    protected $calc;
+
+    public function setUp()
+    {
+        $this->calc = new Calculator();
+    }
 
     public function testResultDefaultsToZero()
     {
-        $calc = new Calculator();
-        $this->assertSame(0, $calc->getResult());
+        $this->assertSame(0, $this->calc->getResult());
     }
 
     public function testAddNumber()
     {
-        $calc = new Calculator();
-        $calc->add(7);
-        $this->assertSame(7, $calc->getResult());
+        $this->calc->add(7);
+        $this->assertSame(7, $this->calc->getResult());
     }
 }
