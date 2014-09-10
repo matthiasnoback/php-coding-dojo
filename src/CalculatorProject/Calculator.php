d3832b0d1119829937e1158f7dd2b3834c1d4781
jeslopcru <jeslopcru@gmail.com>
2014-09-10 23:18:51 +0200

    primer paso

---
+++ src/CalculatorProject/Calculator.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace CalculatorProject;
+
+
+class Calculator
+{
+
+}
---
+++ src/CalculatorProject/Tests/CalculatorTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace CalculatorProject\Tests;
+
+use CalculatorProject\Calculator;
+
+class CalculatorTest extends \PHPUnit_Framework_TestCase
+{
+
+    public function testInstance()
+    {
+        new Calculator();
+    }
+}
