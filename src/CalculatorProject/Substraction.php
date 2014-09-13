640e324d71d5c3101acb10ccf642a66252c6a91c
jeslopcru <jeslopcru@gmail.com>
2014-09-13 13:50:11 +0200

    resta con test e isolation

---
+++ src/CalculatorProject/Substraction.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace CalculatorProject;
+
+class Substraction implements Operation
+{
+    public function run($num, $current)
+    {
+        if (is_null($current)) {
+            return $num;
+        }
+        return $current - $num;
+    }
+} 
